1665eda62381b4f31181c646211c7611a5b068d1
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-24 14:56:33 +1000

    Startup for adding paths

---
+++ startup.m
@@ -0,0 +1,8 @@
+% Add paths required for the project (ignoring hidden, including version control)
+files = dir;
+directories = files([files.isdir]);
+directories(strmatch('.',{files([files.isdir]).name})) = []; % remove hidden
+paths = arrayfun(@(x)fullfile(directories(x).folder,directories(x).name),1:length(directories),'UniformOutput',false);
+for j = 1:length(paths)
+    addpath(genpath(paths{j}))
+end
